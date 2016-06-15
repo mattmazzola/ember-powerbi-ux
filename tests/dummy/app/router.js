89c8f4422163ad00b5a067439be4ab365cf80bcc
Matt Mazzola <mattmazzola@live.com>
2016-06-14 23:48:13 -0700

    Fix navigation bar, add index, browse, favorites, settings, and user routes to demonstrate links.

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -6,6 +6,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('browse');
+  this.route('favorites');
+  this.route('settings');
+  this.route('user');
 });
 
 export default Router;
